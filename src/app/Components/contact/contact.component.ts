4a02dc4695aa5368f136fa017982cbcfb1cccd52
zizoufg <azizmrabet090@gmail.com>
2023-03-29 04:01:00 +0100

    home cards + contact

--- src/app/Components/contact/contact.component.ts
+++ src/app/Components/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { url } from 'inspector';
+import { URL } from 'url';
 
 @Component({
   selector: 'app-contact',
@@ -10,6 +12,12 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const x  = document.getElementById("body");
+    x!.style.backgroundImage = "url(/assets/Contact-Background.jpg)";
+    x!.style.backgroundSize = "100%";
+
+    
+
   }
 
 }
